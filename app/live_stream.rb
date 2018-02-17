56acf7819ba6426394d6f8ec7d7bd6c3b4609fc5
yuya sato <ysato.5654@gmail.com>
2018-02-17 16:51:56 +0800

    update app - add ‘optparse’

--- app/live_stream.rb
+++ app/live_stream.rb
@@ -1,14 +1,59 @@
 #! /opt/local/bin/ruby
 # coding: utf-8
 
+require 'optparse'
+
+require File.expand_path(File.dirname(__FILE__) + '/../lib/tlarb')
+
+class LiveStream
+	attr_reader :environment,
+				:user_id,
+				:movie_id
+
+	DEFAULT_ENVIRONMENT = 'production'
+
+	def initialize
+		opt = OptionParser.new
+
+		params = Hash.new
+
+		opt.on('-e [VAL]', '--environment [VAL]', 'environment (default: production)') { |v| params[:environment] = v }
+		opt.on('-u [VAL]', '--user_id [VAL]', 'user id') { |v| params[:user_id] = v }
+		opt.on('-m [VAL]', '--movie_id [VAL]', 'movie id') { |v| params[:movie_id] = v }
+
+		opt.parse(ARGV)
+
+		@environment = params.key?(:environment) ? params[:environment] : DEFAULT_ENVIRONMENT
+		@user_id = params.key?(:user_id) ? params[:user_id] : ''
+		@movie_id = params.key?(:movie_id) ? params[:movie_id] : ''
+
+		@environment = '' unless Tlarb::ENVIRONMENT.include?(@environment)
+
+		if !@user_id.empty? and !@movie_id.empty?
+			@user_id = ''
+			@movie_id = ''
+		end
+	end
+end
+
 if $0 == __FILE__
-	require File.expand_path(File.dirname(__FILE__) + '/../lib/tlarb')
+	live_stream = LiveStream.new
 
-	env = 'development'
-	#env = 'production'
+	env = live_stream.environment
 
-	#id = {:movie_id => '189037369'}
-	id = {:user_id => '124memetan'}
+	if env.empty?
+		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: option error. usage is 'ruby #{File.basename(__FILE__)} --help'"
+		exit(0)
+	end
+
+	if !live_stream.user_id.empty?
+		id = {:user_id => live_stream.user_id}
+	elsif !live_stream.movie_id.empty?
+		id = {:movie_id => live_stream.movie_id}
+	else
+		STDERR.puts "#{__FILE__}:#{__LINE__}:Error: option error. usage is 'ruby #{File.basename(__FILE__)} --help'"
+		exit(0)
+	end
 
 	# initialize
 	unless Tlarb.initialize(env, id)
@@ -25,8 +70,9 @@ if $0 == __FILE__
 	STDOUT.puts "movie id: #{movie_id}"
 	STDOUT.puts
 
-	# start live stream
+	# create data base
 	stream = Tlarb::Stream.new
+	# start live stream
 	stream.run(movie_id)
 
 	STDOUT.puts "Finish"
